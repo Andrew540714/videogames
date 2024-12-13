e393cac8657bed707cf8e902edd9ce7241262290
unknown <ryanyang1414@gmail.com>
2024-12-13 13:17:54 -0800

    add code

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 import json
 from markupsafe import Markup
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
@@ -9,14 +9,33 @@ def render_main():
 
 @app.route("/money")
 def render_page1():
+
     return render_template('money.html')
+    
+@app.route("/showGameFact")
+def render_page3():
+    with open('video_games.json') as name5:
+        value = json.load(name5)
+        year = ""
+        price = ""
+        console = ""
+        for v in value:
+            if v["Title"] == request.args["game"]:
+               year = "This Game Was Released In " + str(v["Release"]["Year"])
+               price = "This Game Costs $" + str(v["Metrics"]["Used Price"])
+               console = "This Game Was Released On " + str(v["Release"]["Console"])
+
+          
+    value5 = list5()
+    return render_template('graph.html', Year = year, Price = price, Console = console, value22 = value5)
+    
 
 @app.route("/information")
 def render_page2():
-    value5 = list()
+    value5 = list5()
     return render_template('graph.html', value22=value5)
     
-def list():
+def list5():
     with open('video_games.json') as name5:
         value = json.load(name5)
     value5=[]
