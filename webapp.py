1bcfe790fbced95d324bb2e7e94f51ffab95d658
Danieldlz0717 <delaluzdanieljr@gmail.com>
2025-01-08 13:38:21 -0800

    add code

--- webapp.py
+++ webapp.py
@@ -2,16 +2,44 @@ from flask import Flask, url_for, render_template, request
 import json
 from markupsafe import Markup
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
-
+    
 @app.route("/")
 def render_main():
+    price_of_game()
     return render_template('index.html')
 
 @app.route("/money")
 def render_page1():
-
-    return render_template('money.html')
     
+    average_price = price_of_game()
+    return render_template('money.html', points=average_price)
+
+def price_of_game():       
+    with open('video_games.json') as video_games_data:
+        gameData = json.load(video_games_data)
+       
+    years = {}
+    year = 2004
+    numGames = 0
+    totalPrice = 0
+    while year <= 2008:
+        for game in gameData:
+            if game["Release"]["Year"] == year:
+                numGames += 1 
+                totalPrice +=game["Metrics"]["Used Price"]
+        years[year] = totalPrice/numGames
+        year += 1
+    data = ""
+    for key, value in years.items():
+        data = data + Markup("{ x: " + str(key) + ", y: " + str(value) + " },")
+    print(data)                   
+    return data
+    #{ x: 2004, y: 50 },
+
+
+
+
+
 @app.route("/showGameFact")
 def render_page3():
     with open('video_games.json') as name5:
